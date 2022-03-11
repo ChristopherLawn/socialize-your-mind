d1c988ee6e6e238ef4033eb3c4fac48e90c13d82
CLAW <christopher.lawn@yahoo.com>
2022-03-10 23:54:02 -0800

    created thought-controllers & thought api routes

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,7 +1,80 @@
-const { Thought } = require('../models');
+const { Thought, User } = require('../models');
 
 const thoughtController = {
+    // create a new thought
+    createThought({ body }, res) {
+        Thought.create(body)
+            .then(dbThoughtData => {
+                User.findOneAndUpdate(
+                    {_id: body.userId},
+                    { $push: { thoughts: dbThoughtData._id}},
+                    { new: true }
+                )
+                .then(dbUserData => {
+                    if (!dbUserData) {
+                        res.status(404).json({ message: 'No user found with that ID!'});
+                        return;
+                    }
+                    res.json(dbUserData);
+                })
+                .catch(err => res.json(err));
+            })
+            .catch(err => res.status(400).json(err))
+    },
 
+    // get all thoughts
+    getAllThought(req, res) {
+        Thought.find({})
+            .select('-__v')
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            })
+    },
+
+    // get single thought by ID
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.id })
+            .select('-__v')
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with that ID!' });
+                    return;
+                }
+                res.json(dbThoughtData)
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // update thought by ID
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate({ _id: params.id }, body, { new: true })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with that ID!' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+    // delete thought by ID
+    deleteThought({ params }, res) {
+        Thought.findOneAndDelete({ _id: params.id })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with that ID!'});
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.status(400).json(err));
+    }
 };
 
 module.exports = thoughtController;
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,5 +1,26 @@
 const router = require('express').Router();
 
 // a 'const' object that brings in the functions from the controller files and requires the controllers folder
+const {
+    createThought,
+    getAllThought,
+    getThoughtById,
+    updateThought,
+    deleteThought
+} = require('../../controllers/thought-controller');
+
+router
+    .route('/')
+    .get(getAllThought)
+    .post(createThought)
+
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought)
+
+router
+    .route
 
 module.exports = router;
